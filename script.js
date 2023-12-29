aa1b4faa0079170127dc01f256b3444020c6d87c
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2023-12-29 20:04:04 +0100

    implement substract function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let add = function(x, y) {
 return parseFloat(x) + parseFloat(y);
 };
+
+let substract = function (x, y) {
+    return parseFloat(x) - parseFloat(y);
+};
+
+console.log(substract(1.5,3))
