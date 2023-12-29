9417ee0d79ed90009f5e7c5d12c77e44f9435c32
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2023-12-29 20:19:43 +0100

    first operate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let numbner1 = "";
+let number2 = "";
+let operator = "";
+
 let add = function(x, y) {
 return parseFloat(x) + parseFloat(y);
 };
@@ -13,3 +17,24 @@ let multiply = function(x,y){
 let divide = function(x,y){
     return parseFloat(x) / parseFloat(y);
 };
+
+let operate = function(num1, num2, operator) {
+    if (operator === '+' ) {
+        add(num1, num2)
+    }
+    else if (operator === '-') {
+        substract(num1, num2)
+    }
+    else if (operator === '*' ) {
+        multiply(num1, num2)
+    }
+    else if (operator === '/' ) {
+        divide(num1, num2)
+    }
+    else {
+        return "error"
+    }
+
+};
+
+console.log(operate(1,2,'+'))
