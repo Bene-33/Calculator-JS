e04da86a30bf985484a2859f3455028e8ca80a32
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2024-01-11 20:32:48 +0100

    safe input numbers in an array

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-let number1 = [];
+let numberArray = [];
+let number1 = "";
 let number2 = "";
 let operator = "";
 const displayInput = document.querySelector('#inputDisplay');
 const numberSelection = document.querySelectorAll('.number');
 
-// button generate number 
+
+// define first number of calculation
 for(let i = 0; i < numberSelection.length; i++){
     numberSelection[i].addEventListener('click', () => {
-        number1.push(parseInt(numberSelection[i].textContent));
-    })
+        numberArray.push(parseInt(numberSelection[i].textContent));
+        console.log(numberArray)
+    });
 };
 
+//calculation functions
 let add = function(x, y) {
 return parseFloat(x) + parseFloat(y);
 };
@@ -27,6 +31,7 @@ let divide = function(x,y){
     return parseFloat(x) / parseFloat(y);
 };
 
+// make the calculation 
 let operate = function(num1, num2, operator) {
     if (operator === '+' ) {
         return add(num1, num2);
