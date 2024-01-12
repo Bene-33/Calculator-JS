e935dc898b6e8516761fa525b2fe584ace80e9f5
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2024-01-12 19:17:35 +0100

    added some comments for parts that needs to be build

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let numberArray = [];
 let number1 = 1;
 let number2 = 5;
 let operator = "+";
-let solution = 0;
+let solution = 6;
 const displayInput = document.querySelector('#inputDisplay');
 const  displaySolution = document.querySelector('#solutionDisplay');
 const numberSelection = document.querySelectorAll('.number');
@@ -13,18 +13,28 @@ displaySolution.textContent = solution;
 /// still needs to be fixed; for loop working, 
 ///but doesnt´return the number1
 // define first number of calculation
-for(let i = 0; i < numberSelection.length; i++){
-    numberSelection[i].addEventListener('click', () => {
-        numberArray.push(numberSelection[i].textContent);
-        number1 = parseInt(numberArray.join(""));
-               
-    });  
+function selectFirstNumber(){
+    for(let i = 0; i < numberSelection.length; i++){
+        numberSelection[i].addEventListener('click', () => {
+            numberArray.push(numberSelection[i].textContent);
+            number1 = parseInt(numberArray.join(""));
+                
+        });  
+    }; 
 };
 /////////////////////////
 
+//define number2 -like number 1 (if it´s then working)- but only if operator is not ""
+
+function setOperator(){
+    //add eventlistner for each operator and update the operator
+
+};
 
 
 //calculation functions
+//instead of return update the global solution variable
+// round solution to 2 decimal 
 function add(x, y) {
     return parseFloat(x) + parseFloat(y);
 };
@@ -39,9 +49,14 @@ function multiply(x,y){
 
 function divide(x,y){
     return parseFloat(x) / parseFloat(y);
+
+    //add a message if try to divide with 0
 };
 
 // make the calculation 
+// modifie function to check if solution is calculated, then use the solution instead use number1
+//check to implement the call function if after [number operator number] another operator is used to first operate []
+//then use solution + new operator keep doing till equal is pressed
 function operate(num1, num2, operator) {
     if (operator === '+' ) {
         return add(num1, num2);
@@ -59,3 +74,7 @@ function operate(num1, num2, operator) {
         return "error"
     }
 };
+//add eventlistener to equal sign and call operate function
+
+//add AC function to start fresh without any number set 
+
